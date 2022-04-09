5d087df4d353e291f5c91bcf0e6d825f26edbac9
Caleb Curry <caleb@calebcurry.com>
2022-04-09 10:14:49 -0400

    add localhost

--- files/settings.py
+++ files/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@^*k7x=0a34@+*eu37hn)v72)nky3n*6%@tmbq%y+u=dg_pfg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
