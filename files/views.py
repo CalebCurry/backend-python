daff7c85f65fe379a0aa42377743426384decbf5
Caleb Curry <caleb@calebcurry.com>
2022-04-08 22:29:18 -0400

    basic first template

--- files/views.py
+++ files/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('Hello there')
 
+data = [
+    {'name': 'image1.jpg', 'type':'jpg'},
+    {'name': 'notes.txt', 'type': 'txt'},
+    {'name': 'image2.jpg', 'type': 'jpg'}
+]
+
 def files(request):
-    return render(request, 'files/files.html', {'data': 'test-data'})
+    return render(request, 'files/files.html', {'files': data})
