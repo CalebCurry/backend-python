9a628c393c3356b340b66435c8cbf715f1edbde0
Caleb Curry <caleb@calebcurry.com>
2022-04-08 22:05:34 -0400

    Getting ready for lesson 9

--- files/urls.py
+++ files/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('files/', views.files, name='files')
 ]
--- files/views.py
+++ files/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('Hello there')
+
+def files(request):
+    return render(request, 'files/files.html', {'data': 'test-data'})
