7624fe767309e9a7f4b93da58c339fe7e3265d31
Caleb Curry <caleb@calebcurry.com>
2022-04-09 12:04:36 -0400

    string representation of our model

--- files/models.py
+++ files/models.py
@@ -4,3 +4,6 @@ class File(models.Model):
     name = models.CharField(max_length=1024)
     file_type = models.CharField(max_length=1024)
     upload_timestamp = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
