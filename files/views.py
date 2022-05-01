e8b6774b77764ed6c37eb6e354b6384d21c28400
Caleb Curry <caleb@calebcurry.com>
2022-05-01 14:42:54 -0400

    start of 29

--- files/views.py
+++ files/views.py
@@ -15,16 +15,29 @@ from rest_framework.decorators import api_view
 def index(request):
     return render(request, 'files/index.html')
 
+@api_view(['GET'])
 def files(request, format=None):
     data = File.objects.all()
     serializer = FileSerializer(data, many=True)
-    return JsonResponse({'files': serializer.data})
+    return Response({'files': serializer.data})
 
-@api_view(['GET'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 def file(request, file_id, format=None):
     try:
         data = File.objects.get(pk=file_id)
     except File.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = FileSerializer(data)
-    return Response({'file': serializer.data})
+    if request.method == 'GET':
+        serializer = FileSerializer(data)
+        return Response({'file': serializer.data})
+
+    elif request.method == 'PATCH':
+        serializer = FileSerializer(data, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
